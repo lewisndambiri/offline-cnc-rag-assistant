ed608940dc36fae6ececc5d3b74182e724f044f5
Lewis NDAMBIRI <ndambirilewis@gmail.com>
2026-05-23 14:06:03 +0200

    Add __init__.py for generation module

---
+++ src/generation/_init_.py
@@ -0,0 +1,3 @@
+from cnc_rag.generation.ollama import answer_with_ollama, format_source_list, stream_answer_with_ollama
+
+__all__ = ["answer_with_ollama", "format_source_list", "stream_answer_with_ollama"]
